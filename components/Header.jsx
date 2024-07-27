97a494e9eb8d5f44e73523907812deed7b1a6118
Kacem Ettahali <101kacem101@gmail.com>
2024-07-27 00:36:24 -0500

    Fix icon

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,32 +7,35 @@ import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
-    <header className="py-8 xl:py-12 text-white">
-      <div className="container mx-auto flex justify-between items-center">
-        {/* logo */}
-        <Link href="/" legacyBehavior>
-          <a className="flex items-center gap-2">
-            <img src="/icon.ico" alt="Icon" className="w-12 h-12" />
-            <h1 className="text-4xl font-semibold flex items-center">
-              Kacem<span className="text-accent">.</span>
-            </h1>
-          </a>
-        </Link>
-
-        {/* desktop nav & hire me button */}
-        <div className="hidden xl:flex items-center gap-8">
-          <Nav />
-          <Link href="/contact" legacyBehavior>
-            <Button>Hire me</Button>
+    <>
+      <link rel="icon" href="/icon.ico" />
+      <header className="py-8 xl:py-12 text-white">
+        <div className="container mx-auto flex justify-between items-center">
+          {/* logo */}
+          <Link href="/" legacyBehavior>
+            <a className="flex items-center gap-2">
+              <img src="/icon.ico" alt="Icon" className="w-10 h-10" />
+              <h1 className="text-4xl font-semibold flex items-center">
+                Kacem<span className="text-accent">.</span>
+              </h1>
+            </a>
           </Link>
-        </div>
 
-        {/* mobile nav */}
-        <div className="xl:hidden">
-          <MobileNav />
+          {/* desktop nav & hire me button */}
+          <div className="hidden xl:flex items-center gap-8">
+            <Nav />
+            <Link href="/contact" legacyBehavior>
+              <Button>Hire me</Button>
+            </Link>
+          </div>
+
+          {/* mobile nav */}
+          <div className="xl:hidden">
+            <MobileNav />
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 };
 
